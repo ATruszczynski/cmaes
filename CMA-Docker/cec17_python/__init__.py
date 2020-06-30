8f7b6118cb7af425931756a086956d01538810cf
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2020-06-30 14:02:41 +0200

    Example use

--- CMA-Docker/cec17_python/__init__.py
+++ CMA-Docker/cec17_python/__init__.py
@@ -1,6 +1,16 @@
 from cec17_functions import cec17_test_func
+from cma import *
 
-print("desu")
+class Wrapper:
+	def __init__(self, dims, func):
+		self.dims = dims
+		self.func = func
+	
+	def compute(self, arg):
+		f = [0]
+		cec17_test_func(arg, f, self.dims, 1, self.func)
+		return f[0]
+
 # x: Solution vector
 x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 # nx: Number of dimensions
@@ -14,3 +24,17 @@ f = [0]
 
 cec17_test_func(x, f, nx, mx, func_num)
 print(f[0])
+
+wrapper = Wrapper(10, 1)
+
+res = fmin(
+		wrapper.compute,
+		[0,0],
+		1, 
+		options = {'verbose':-9}
+	  )
+
+print("Best solution: %s" % res[0])
+print("Best value: %s" % res[1])
+
+
