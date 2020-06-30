892bf7d84f53b0e637b69b6627256a09033bfadd
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2020-06-30 11:32:24 +0200

    All

---
+++ CMA-Docker/cec17_python/__init__.py
@@ -0,0 +1,16 @@
+from cec17_functions import cec17_test_func
+
+print("desu")
+# x: Solution vector
+x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# nx: Number of dimensions
+nx = 10
+# mx: Number of objective functions
+mx = 1
+# func_num: Function number
+func_num = 1
+# Pointer for the calculated fitness
+f = [0]
+
+cec17_test_func(x, f, nx, mx, func_num)
+print(f[0])
