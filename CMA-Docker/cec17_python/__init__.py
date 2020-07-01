4a4f6f76ea3e830979497663273701c316a4638c
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2020-07-01 16:03:07 +0200

    Incomplete loop

--- CMA-Docker/cec17_python/__init__.py
+++ CMA-Docker/cec17_python/__init__.py
@@ -5,12 +5,12 @@ import os
 
 class Wrapper:
 	def __init__(self, dims, func):
-		self.dims = dims
+		self.dims = dims	
 		self.func = func
 	
 	def compute(self, arg):
 		f = [0]
-		cec17_test_func(arg, f, self.dims, 1, self.func)
+		cec17_test_func(arg, f, self.dims, 10, self.func)
 		return f[0]
 
 # x: Solution vector
@@ -30,25 +30,26 @@ print(f[0])
 
 wrapper = Wrapper(10, 1)
 
-adapt_method = [CMAAdaptSigmaMedianImprovement, CMAAdaptSigmaTPA, CMAAdaptSigmaCSA]
-dims = [2]
+adapt_method = [CMAAdaptSigmaMedianImprovement]#, CMAAdaptSigmaTPA, CMAAdaptSigmaCSA]
+dims = [10]
 funcs = [i for i in range(1, 31) if i not in [2]]
 sigma = 1
 start_point = [0]
 bounds = [-100, 100]
 verbosity = -1
 verblog = 11
+seed = None #!!! Change to 0 or None before tests
 
 variants = [(a, b, c) for a in adapt_method for b in dims for c in funcs]
 
 for v in variants:
     am = v[0]
     ams = ""
-    if(isinstance(am, CMAAdaptSigmaMedianImprovement)):
+    if(am == CMAAdaptSigmaMedianImprovement):
         ams = "Med"
-    if(isinstance(am, CMAAdaptSigmaTPA)):
+    if(am == CMAAdaptSigmaTPA):
         ams = "TPA"
-    if(isinstance(am, CMAAdaptSigmaCSA)):
+    if(am == CMAAdaptSigmaCSA):
         ams = "CSA"
     dim = v[1]
     func = v[2]
@@ -60,6 +61,7 @@ for v in variants:
                 dim * start_point,
                 sigma,
                 options={
+			    'seed': seed,
                             'AdaptSigma': am,
                             'bounds': [-100, 100],
                             'verbose': verbosity,
@@ -70,15 +72,6 @@ for v in variants:
 
 
 
-res = fmin(
-		wrapper.compute, # funkcja minimalizowana
-		10 * [-100], # początkowe rozwiązanie
-		1, # długość kroku sigma
-		options = {'verbose':-1, # -9 żeby się zamknął
-                'bounds': [-100, 100] , # ograniczenia. Wartości recyklowane, jeśli wyjdziesz za indeks. Początkowe rozwiązanie musi się w nich mieścić, bo inaczej się obrazi
-			   'verb_log': 1, 
-			   'verb_filenameprefix': os.path.join(CMADataLogger.default_prefix, "Test_" + str(wrapper.func) + "_" + str(wrapper.dims), "")} # słownik z opcjami algorytmu
-	  )
 
 
 
