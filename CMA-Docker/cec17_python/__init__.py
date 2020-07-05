745f6156a1f0d3888680313438bad7c35d191a4b
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2020-07-05 15:41:54 +0200

    Reversing git derps

--- CMA-Docker/cec17_python/__init__.py
+++ CMA-Docker/cec17_python/__init__.py
@@ -60,13 +60,14 @@ def job(v):
                 dim * start_point,
                 sigma,
                 options={
-			    'seed': seed,
-                            'AdaptSigma': am,
-                            'bounds': bounds,
-                            'verbose': verbosity,
-                            'verb_log': verblog,
-			    'verb_filenameprefix': folderName 
-                        } # słownik z opcjami algorytmu
+			    'seed': rep, #probably will generate the same starting pop for each adptation method
+                'maxfevals':  dim * 1e4, #maximum function evaluations
+                'AdaptSigma': am,
+                'bounds': bounds,
+                'verbose': verbosity,
+                'verb_log': verblog,
+			    'verb_filenameprefix': folderName,
+                } # słownik z opcjami algorytmu
               )
     for filee in filesToRemove:
         os.remove(os.path.join(folderName, filee))
@@ -81,7 +82,6 @@ if __name__ == '__main__':
 end = timer()
 print("\nExecution time:")
 print(end - start)
-
 
 
 
